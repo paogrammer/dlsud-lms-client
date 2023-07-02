4b86cff0b3b900837873bb64853ed046555de292
paogrammer <devpsalud@gmail.com>
2023-07-02 14:45:51 +0800

    fixes

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,27 +1,27 @@
-import { connectToDatabase } from './db';
-import User from '../../models/user'; // Import the user schema
+// import { connectToDatabase } from './db';
+// import User from '../../models/user'; // Import the user schema
 
-// Connect to MongoDB
-connectToDatabase();
+// // Connect to MongoDB
+// connectToDatabase();
 
-// API route for user registration
-export default async function registerUser(req, res) {
-  if (req.method === 'POST') {
-    try {
-      const { email, password, name } = req.body;
+// // API route for user registration
+// export default async function registerUser(req, res) {
+//   if (req.method === 'POST') {
+//     try {
+//       const { email, password, name } = req.body;
 
-      // Create a new user using the schema
-      const user = new User({ email, password, name });
+//       // Create a new user using the schema
+//       const user = new User({ email, password, name });
 
-      // Save the user to the database
-      await user.save();
+//       // Save the user to the database
+//       await user.save();
 
-      res.status(201).json({ message: 'User registered successfully!' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server error' });
-    }
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
-}
+//       res.status(201).json({ message: 'User registered successfully!' });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ message: 'Server error' });
+//     }
+//   } else {
+//     res.status(405).json({ message: 'Method not allowed' });
+//   }
+// }
